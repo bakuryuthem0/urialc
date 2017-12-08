6f7028265580f6f9713cdaa32b76a41fee7be13e
Carlucho <shenlong_12@hotmailcom>
2017-12-08 12:26:54 -0400

    session facade added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
 
 class AuthController extends Controller {
 	public function __construct()
